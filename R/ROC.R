78b6d15ca2efd1d0156d48febb397837ebf27f33
Toby Dylan Hocking <toby.hocking@r-project.org>
2017-06-19 21:49:23 +0000

    version 2017.06.08

---
+++ R/ROC.R
@@ -0,0 +1,183 @@
+WeightedROC <- structure(function
+### Compute a weighted ROC curve.
+(guess,
+### Numeric vector of scores.
+ label,
+### True positive/negative labels. A factor with 2 unique values, or
+### integer/numeric with values all in {0=negative,1=positive} or
+### {1=negative,2=positive} or {-1=negative,1=positive}.
+ weight=rep(1, length(label))
+### Positive weights, by default 1.
+ ){
+  if(is.factor(label)){
+    label <- as.integer(label)
+  }
+  stopifnot(is.numeric(label))
+  label.tab <- table(label)
+  if(length(label.tab) == 1){
+    print(label.tab)
+    stop("only one label value")
+  }
+  if(all(label %in% c(0, 1))){
+    label[label==0] <- -1
+  }
+  if(all(label %in% c(1, 2))){
+    label[label==1] <- -1
+    label[label==2] <- 1
+  }
+  ## by now, label must be in {-1,1}.
+  stopifnot(label %in% c(-1,1))
+  ## guess must be real.
+  stopifnot(is.numeric(guess))
+  stopifnot(length(label) == length(guess))
+  if(any(is.na(guess))){
+    stop("ROC curve undefined for NA guess")
+  }
+  ## weights must be positive.
+  stopifnot(is.numeric(weight))
+  stopifnot(length(label) == length(weight))
+  stopifnot(weight > 0)
+  ## order by guess to use cumsum.
+  ord <- order(guess)
+  y <- label[ord]
+  w <- weight[ord]
+  y.hat <- guess[ord]
+  is.positive <- y == 1
+  is.negative <- y == -1
+  w.positive <- w.negative <- w
+  w.positive[is.negative] <- 0
+  w.negative[is.positive] <- 0
+  cum.positive <- cumsum(w.positive)
+  cum.negative <- cumsum(w.negative)
+  is.end <- c(diff(y.hat) != 0, TRUE)
+  n <- length(y)
+  threshold <- c(y.hat[is.end], Inf)
+  total.positive <- cum.positive[n]
+  total.negative <- cum.negative[n]
+  FN <- c(0, cum.positive[is.end])
+  FNR <- FN/total.positive
+  TPR <- 1-FNR
+  TN <- c(0, cum.negative[is.end])
+  FP <- total.negative - TN
+  FPR <- FP/total.negative
+  d <- data.frame(TPR, FPR, threshold, FN, FP)
+  d
+### data.frame with true positive rate (TPR), false positive rate
+### (FPR), weighted false positive count (FP), weighted false negative
+### count (FN), and threshold (smallest guess classified as positive).
+}, ex=function(){
+
+  ## WeightedROC can compute ROC curves for data sets with variable
+  ## weights.
+  library(WeightedROC)
+  y <- c(-1, -1, 1, 1, 1)
+  w <- c(1, 1, 1, 4, 5)
+  y.hat <- c(1, 2, 3, 1, 1)
+  tp.fp <- WeightedROC(y.hat, y, w)
+  library(ggplot2)
+  ggplot()+
+    geom_path(aes(FPR, TPR), data=tp.fp)+
+    coord_equal()
+
+  ## The FN/FP columns can be used to plot weighted error as a
+  ## function of threshold.
+  error.fun.list <- list(
+    FN=function(df)df$FN,
+    FP=function(df)df$FP,
+    errors=function(df)with(df, FP+FN)
+    )
+  all.error.list <- list()
+  for(error.type in names(error.fun.list)){
+    error.fun <- error.fun.list[[error.type]]
+    all.error.list[[error.type]] <-
+      data.frame(tp.fp, error.type, weighted.error=error.fun(tp.fp))
+  }
+  all.error <- do.call(rbind, all.error.list)
+  fp.fn.colors <- c(FP="skyblue",
+                    FN="#E41A1C",
+                    errors="black")
+  ggplot()+
+    scale_color_manual(values=fp.fn.colors)+
+    geom_line(aes(threshold, weighted.error, color=error.type),
+              data=all.error)
+
+  library(ROCR)
+  library(pROC)
+  library(microbenchmark)
+  data(ROCR.simple, envir=environment())
+  ## Compare speed and plot ROC curves for the ROCR example data set.
+  microbenchmark(WeightedROC={
+    tp.fp <- with(ROCR.simple, WeightedROC(predictions, labels))
+  }, ROCR={
+    pred <- with(ROCR.simple, prediction(predictions, labels))
+    perf <- performance(pred, "tpr", "fpr")
+  }, pROC.1={
+    proc <- roc(labels ~ predictions, ROCR.simple, algorithm=1)
+  }, pROC.2={
+    proc <- roc(labels ~ predictions, ROCR.simple, algorithm=2)
+  }, pROC.3={
+    proc <- roc(labels ~ predictions, ROCR.simple, algorithm=3)
+  }, times=10)
+  perfDF <- function(p){
+    data.frame(FPR=p@x.values[[1]], TPR=p@y.values[[1]], package="ROCR")
+  }
+  procDF <- function(p){
+    data.frame(FPR=1-p$specificities, TPR=p$sensitivities, package="pROC")
+  }
+  roc.curves <- rbind(
+    data.frame(tp.fp[, c("FPR", "TPR")], package="WeightedROC"),
+    perfDF(perf),
+    procDF(proc))
+  ggplot()+
+    geom_path(aes(FPR, TPR, color=package, linetype=package),
+              data=roc.curves, size=1)+
+    coord_equal()
+  
+  ## Compare speed and plot ROC curves for the pROC example data set.
+  data(aSAH, envir=environment())
+  microbenchmark(WeightedROC={
+    tp.fp <- with(aSAH, WeightedROC(s100b, outcome))
+  }, ROCR={
+    pred <- with(aSAH, prediction(s100b, outcome))
+    perf <- performance(pred, "tpr", "fpr")
+  }, pROC.1={
+    proc <- roc(outcome ~ s100b, aSAH, algorithm=1)
+  }, pROC.2={
+    proc <- roc(outcome ~ s100b, aSAH, algorithm=2)
+  }, pROC.3={
+    proc <- roc(outcome ~ s100b, aSAH, algorithm=3)
+  }, times=10)
+  roc.curves <- rbind(
+    data.frame(tp.fp[, c("FPR", "TPR")], package="WeightedROC"),
+    perfDF(perf),
+    procDF(proc))
+  ggplot()+
+    geom_path(aes(FPR, TPR, color=package, linetype=package),
+              data=roc.curves, size=1)+
+    coord_equal()
+  
+  ## Compute a small ROC curve with 1 tie to show the diagonal.
+  y <- c(-1, -1, 1, 1)
+  y.hat <- c(1, 2, 3, 1)
+  microbenchmark(WeightedROC={
+    tp.fp <- WeightedROC(y.hat, y)
+  }, ROCR={
+    pred <- prediction(y.hat, y)
+    perf <- performance(pred, "tpr", "fpr")
+  }, pROC.1={
+    proc <- roc(y ~ y.hat, algorithm=1)
+  }, pROC.2={
+    proc <- roc(y ~ y.hat, algorithm=2)
+  }, pROC.3={
+    proc <- roc(y ~ y.hat, algorithm=3)
+  }, times=10)
+  roc.curves <- rbind(
+    data.frame(tp.fp[, c("FPR", "TPR")], package="WeightedROC"),
+    perfDF(perf),
+    procDF(proc))
+  ggplot()+
+    geom_path(aes(FPR, TPR, color=package, linetype=package),
+              data=roc.curves, size=1)+
+    coord_equal()
+  
+})
---
+++ inst/doc/Speed.R
@@ -0,0 +1,113 @@
+### R code from vignette source 'Speed.Rnw'
+
+###################################################
+### code chunk number 1: data
+###################################################
+library(ElemStatLearn)
+data(spam)
+is.label <- names(spam) == "spam"
+X <- as.matrix(spam[,!is.label])
+y <- spam[,is.label]
+set.seed(1)
+train.i <- sample(nrow(spam), nrow(spam)/2)
+sets <- 
+  list(train=list(features=X[train.i, ], label=y[train.i]),
+       test=list(features=X[-train.i, ], label=y[-train.i]))
+str(sets)
+
+
+###################################################
+### code chunk number 2: model
+###################################################
+library(glmnet)
+system.time({
+  fit <- cv.glmnet(sets$train$features, sets$train$label, family="binomial")
+})
+
+
+###################################################
+### code chunk number 3: test-ROC-time
+###################################################
+library(WeightedROC)
+library(ROCR)
+library(pROC)
+library(microbenchmark)
+set <- sets$test
+guess <- predict(fit, set$features)
+microbenchmark(WeightedROC={
+  wroc <- WeightedROC(guess, set$label)
+}, ROCR={
+  pred <- prediction(guess, set$label)
+  perf <- performance(pred, "tpr", "fpr")
+}, pROC={
+  proc <- roc(set$label, guess, algorithm=2)
+})
+
+
+
+###################################################
+### code chunk number 4: test-ROC-curves
+###################################################
+perfDF <- function(p){
+  data.frame(FPR=p@x.values[[1]], TPR=p@y.values[[1]], package="ROCR")
+}
+procDF <- function(p){
+  data.frame(FPR=1-p$specificities, TPR=p$sensitivities, package="pROC")
+}
+roc.curves <- 
+  rbind(data.frame(wroc[,c("FPR", "TPR")], package="WeightedROC"),
+        perfDF(perf), procDF(proc))
+library(ggplot2)
+rocPlot <- ggplot()+
+  geom_path(aes(FPR, TPR, color=package, linetype=package),
+            data=roc.curves, size=1)+
+  coord_equal()
+print(rocPlot)
+
+
+
+###################################################
+### code chunk number 5: scaling
+###################################################
+stats.by.size.expr <- list()
+ms.by.size <- list()
+for(size in c(400, 800, 1200, 1500, 2000, 2300)){
+  indices <- seq(1, length(set$label), l=size)
+  y <- set$label[indices]
+  y.hat <- guess[indices]
+  this.size <- microbenchmark(WeightedROC={
+    wroc <- WeightedROC(y.hat, y)
+  }, ROCR={
+    pred <- prediction(y.hat, y)
+    perf <- performance(pred, "tpr", "fpr")
+  }, pROC.1={
+    proc <- roc(y, y.hat, algorithm=1)
+  }, pROC.2={
+    proc <- roc(y, y.hat, algorithm=2)
+  })
+  this.size$milliseconds <- this.size$time/1e6
+  ms.by.size[[paste(size)]] <- data.frame(size, this.size)
+  this.by.expr <- split(this.size, this.size$expr)
+  for(expr in names(this.by.expr)){
+    stats <- with(this.by.expr[[expr]], {
+      data.frame(median=median(milliseconds),
+                 q25=quantile(milliseconds, 0.25),
+                 q75=quantile(milliseconds, 0.75))
+    })
+    stats.by.size.expr[[paste(size, expr)]] <- data.frame(size, expr, stats)
+  }
+}
+ms <- do.call(rbind, ms.by.size)
+algo.stats <- do.call(rbind, stats.by.size.expr)
+timePlot <- ggplot()+
+  geom_ribbon(aes(size, ymin=q25, ymax=q75, fill=expr), 
+              data=algo.stats, alpha=1/2)+
+  geom_line(aes(size, median, color=expr), data=algo.stats, size=2)+
+  geom_point(aes(size, milliseconds, color=expr), data=ms, pch=1)+
+  ylab("milliseconds")+
+  xlab("data set size")+
+  ggtitle("mean +/- 1 standard deviation")
+print(timePlot)
+
+
+
